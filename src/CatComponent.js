45a14c95be85983cd2cfa9c5a7b8d8179e0ea1dc
luis <foreverluiscastro@gmail.com>
2021-04-13 19:36:20 -0400

    Done.

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -4,6 +4,7 @@ export default class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
+        <h3>{this.props.name}</h3>
         <img src="/cat.gif" />
       </div>
     );
